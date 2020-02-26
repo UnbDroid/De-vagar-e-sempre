1b121f18d21553725d8de32dfc6e96d79b1f4d9e
waliffcordeiro <43971264+waliffcordeiro@users.noreply.github.com>
2020-02-26 15:08:32 -0300

    Update identifyObject.py

--- test/identifyObject.py
+++ test/identifyObject.py
@@ -1,42 +1,55 @@
-import numpy as np
-import cv2
+import cv2 as cv
+from teste2 import pictureCapture
 
-def indetifyObject():
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+PENTAGON = 1
+TRIANGLE = 2
+SQUARE = 3
+STAR = 4
+HALF_CIRCLE = 5
+CIRCLE = 6
 
-    ret,thresh = cv2.threshold(gray,127,255,1)
+def indetifyObject(img):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    contours,h = cv2.findContours(thresh,1,2)
+    ret, thresh = cv.threshold(gray, 127, 255, 1)
+
+    contours, h = cv.findContours(thresh, 1, 2)
 
     for cnt in contours:
-        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        approx = cv.approxPolyDP(cnt, 0.01 * cv.arcLength(cnt, True), True)
         print(len(approx))
-    if len(approx)==5:
+    if len(approx) == 5:
         print("pentagon")
-        cv2.drawContours(img,[cnt],0,255,-1)
+        cv.drawContours(img, [cnt], 0, 255, -1)
         return 1
-    elif len(approx)==3:
+    elif len(approx) == 3:
         print("triangle")
-        cv2.drawContours(img,[cnt],0,(0,255,0),-1)
+        cv.drawContours(img, [cnt], 0, (0, 255, 0), -1)
         return 2
-    elif len(approx)==4:
+    elif len(approx) == 4:
         print("square")
-        cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+        cv.drawContours(img, [cnt], 0, (0, 0, 255), -1)
         return 3
+    elif len(approx) == 5:
+        print("star")
+        cv.drawContours(img, [cnt], 0, (0, 0, 255), -1)
+        return 4
     elif len(approx) == 9:
         print("half-circle")
-        cv2.drawContours(img,[cnt],0,(255,255,0),-1)
-        return 4
+        cv.drawContours(img, [cnt], 0, (255, 255, 0), -1)
+        return 5
     elif len(approx) > 15:
         print("circle")
-        cv2.drawContours(img,[cnt],0,(0,255,255),-1)
-        return 5
+        cv.drawContours(img, [cnt], 0, (0, 255, 255), -1)
+        return 6
+
+
+
 if __name__ == '__main__':
     pictureCapture(1)
     img = cv.imread("1.jpeg")
-    print("resultado final: " + str(len(approx)))
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    
+    form = indetifyObject(img)
+    print(form)
+    cv.imshow('img', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
