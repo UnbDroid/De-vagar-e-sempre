ff21e472a40ec420b434e58cfe5f679a88ee7fe3
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2020-03-05 10:10:11 -0300

    Adicionar controle com gyro

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,11 @@
 
 //bool inicio = false;
 static int prev_a, a = 1, cont = 5;
-unsigned long val_us[2]; 
+unsigned long val_us[2];
+int cont_b_b;
+int controle[2];
+int err_controle;
+float last_angle;
 
 void setup()
 {
@@ -16,6 +20,10 @@ void setup()
   para(1000);
   setup_us();
   startGyro();
+  cont_b_b = 0;
+  controle[0] = MEDIO_PWM;
+  controle[1] = MEDIO_PWM;
+  err_controle = 0;
 }
 
 void loop() {
@@ -47,56 +55,72 @@ void loop() {
   }
   prev_a = a;
   a = ir_read();
+  last_angle = getGyro();
   Serial.println(a);
-  switch (a) {
-    case 0: //Branco nos dois
-      led(0, 0);
-      //if (prev_a != 0) 
-      motor(270, 250, RETO_TIME);
-      motor(115, 85, 10);
-      para(300);
-      break;
-    case 1: //Preto na direita
-      led(0, 1);
-      para(300);
-      //if(ir_read() != 1) break;
-      motor(250, -250, GIRO_TIME);
-      motor(140,-90, GIRO_TIME);
-      para(200);
-      break;
-    case 2: //Preto na esquerda
-      led(1, 0);
-      para(200);
-      //if(ir_read() != 2) break;
-      motor(-250, 250, GIRO_TIME);
-      motor(-90,140,GIRO_TIME);
-      para(200);
-      break;
-    case 3: //Preto nos dois
-      led(1, 1);
-      if(prev_a == 1) {
-
-
-      motor(250, -250, GIRO_TIME);
-      motor(140,-90, GIRO_TIME);
-      para(200);
-        
-      } else if(prev_a == 2) {
-
-      motor(-250, 250, GIRO_TIME);
-      motor(-90,140,GIRO_TIME);
-      para(200);
-
-      } else {
-        motor(-260, -200, RETO_TIME);
-        motor(-150,-80,10);
+  if(cont_b_b < 2){
+    switch (a) {
+      case 0: //Branco nos dois
+        ++cont_b_b;
+        led(0, 0);
+        //if (prev_a != 0) 
+        motor(270, 250, RETO_TIME);
+        motor(115, 85, 10);
         para(300);
-      }
-      break;
-    default:
-      motor(0, 0, 3000);
-      break;
-  }
-  
+        break;
+      case 1: //Preto na direita
+        led(0, 1);
+        para(300);
+        //if(ir_read() != 1) break;
+        motor(250, -250, GIRO_TIME);
+        motor(140,-90, GIRO_TIME);
+        para(200);
+        break;
+      case 2: //Preto na esquerda
+        led(1, 0);
+        para(200);
+        //if(ir_read() != 2) break;
+        motor(-250, 250, GIRO_TIME);
+        motor(-90,140,GIRO_TIME);
+        para(200);
+        break;
+      case 3: //Preto nos dois
+        led(1, 1);
+        if(prev_a == 1) {
 
+
+        motor(250, -250, GIRO_TIME);
+        motor(140,-90, GIRO_TIME);
+        para(200);
+          
+        } else if(prev_a == 2) {
+
+        motor(-250, 250, GIRO_TIME);
+        motor(-90,140,GIRO_TIME);
+        para(200);
+
+        } else {
+          motor(-260, -200, RETO_TIME);
+          motor(-150,-80,10);
+          para(300);
+        }
+        break;
+      default:
+        motor(0, 0, 3000);
+        break;
+    }
+  }
+  else{
+    para(100);
+    motor(TRANCO_PWM, TRANCO_PWM, 10);
+    while(a == 0){
+      segue_reto(controle, &err_controle, 0, last_angle);
+      a = ir_read();
+      read_us(val_us);
+      if(val_us[0] <= 20 && val_us[0] != 0){
+        para(20);
+        break;
+      }
+    }
+    cont_b_b = 0;
+  }
 }
